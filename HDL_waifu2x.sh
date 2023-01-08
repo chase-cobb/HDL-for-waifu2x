ea9f78d376338cd9dbda19ba9b6d85d98528d726
Chase Cobb <chase@8bitroots.com>
2023-01-08 14:57:12 -0600

    fixed race conditions in ffmpeg commands while looping

--- HDL_waifu2x.sh
+++ HDL_waifu2x.sh
@@ -89,32 +89,60 @@ for value in "${filesToConvert[@]}"
 do
     local inputFile=$value
     local currentDirectory=$(cd "$(dirname "$inputFile")"; pwd -P)
+
+    # if this is a MKV, move it into a MP4 container
+    if [[ ${inputFile##*.} == "mkv" ]] || [[ ${inputFile##*.} == "MKV" ]]
+    then
+
+        # TODO : kill any orphaned instances of ffmpeg
+
+        echo ""
+        echo "Converting MKV to MP4 -> $inputFile"
+        local fileName=$(basename "$inputFile")
+        fileName="${fileName%.*}"
+        local newFile="${currentDirectory}/${fileName}.mp4"
+        ffmpeg -nostdin -i "$inputFile" -codec copy "$newFile"
+        wait
+        inputFile="$newFile"
+        echo "Converting MKV to MP4 Complete -> $newFile"
+        echo ""
+    fi
+
     local outputDirectory="$currentDirectory/upscaled"
     local tempDirectory="$currentDirectory/upscaled/temp"
+    mkdir $outputDirectory
+    mkdir $tempDirectory
     local fileName="$(basename $inputFile)"
     local tempVideo="$tempDirectory/$fileName"
     local tempAudio="$tempDirectory/audio.mov"
     local inputFileExtension="${fileName##*.}"
-    mkdir $outputDirectory
-    mkdir $tempDirectory
 
     if [[ $performIVTC == 1 ]]
     then
         local detelecineOut="${tempDirectory}/ivtc_${fileName}"
         ffmpeg -i $inputFile -vf fieldmatch,yadif=deint=interlaced,decimate -preset veryslow -crf 13 $detelecineOut
+        wait
         inputFile="$detelecineOut"
     fi
 
+    sleep 3s
+
     #echo "========================================================"
-    local frameRateFraction=$(ffprobe -v error -select_streams v -of default=noprint_wrappers=1:nokey=1 -show_entries stream=r_frame_rate $inputFile)
+    #local frameRateFraction=$(ffprobe -v error -select_streams v -of default=noprint_wrappers=1:nokey=1 -show_entries stream=r_frame_rate $inputFile) // HACK BELOW using the frame rate from the original mkv
+    local frameRateFraction=$(ffprobe -v error -select_streams v -of default=noprint_wrappers=1:nokey=1 -show_entries stream=r_frame_rate $value)
     local frameRateTruncated=$(echo "scale=3; $frameRateFraction" | bc)
     local inputResolutionWidth=$(ffprobe -v error -select_streams v:0 -show_entries stream=width -of csv=s=x:p=0 $inputFile)
+    inputResolutionWidth=${inputResolutionWidth: : -1}
     local inputResolutionHeight=$(ffprobe -v error -select_streams v:0 -show_entries stream=height -of csv=s=x:p=0 $inputFile)
+    inputResolutionHeight=${inputResolutionHeight: : -1}
 
     #Make sure output is scaled according to the upscaler arguments
     GetUpscaleResolutionScalar
     local outputResolutionScalar=$?
+    #local outputResolutionWidth=$(echo "$(( inputResolutionWidth * $outputResolutionScalar ))")
+    #local outputResolutionHeight=$(echo "$(( inputResolutionHeight * $outputResolutionScalar ))")
     local outputResolution=$(echo "$(( inputResolutionWidth * $outputResolutionScalar ))x$(( inputResolutionHeight * $outputResolutionScalar ))")
+    #local outputResolution="${outputResolutionWidth}x${outputResolutionHeight}"
 
     echo ""
     echo "File info =============================="
@@ -131,13 +159,19 @@ do
     ffmpeg -i "$inputFile" -pix_fmt rgba64le -f rawvideo -loglevel quiet - |
         waifu2x "${waifu2xArguments[@]}" --raw --width $inputResolutionWidth --height $inputResolutionHeight --16-bit |
         ffmpeg -f rawvideo -pix_fmt rgba64le -s:v $outputResolution -r $frameRateTruncated -i - -c:v libx264 "$tempVideo"
+    wait
+    sleep 2s
 
     #Copy audio from input video to output video
     ffmpeg -i "$tempVideo" -i "$inputFile" -c:v copy -c:a copy -map 0:v:0 -map 1:a "$outputDirectory/$fileName"
+    wait
     rm -r $tempDirectory
+    
 
     echo ""
     unset streamsArray
+
+    sleep 2s
 done
 
 #Wait for user interaction to exit script
